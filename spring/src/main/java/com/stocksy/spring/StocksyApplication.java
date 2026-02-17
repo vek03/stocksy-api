47fac77d04607dbb20740c4f42142bfb093422c0
vek03 <vektromboni@gmail.com>
2026-02-16 21:28:04 -0300

    feat: initial log

--- spring/src/main/java/com/stocksy/spring/StocksyApplication.java
+++ spring/src/main/java/com/stocksy/spring/StocksyApplication.java
@@ -1,13 +1,24 @@
 package com.stocksy.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-public class StocksyApplication {
+@EnableCaching
+public class StocksyApplication implements CommandLineRunner {
+
+	private static final Logger LOG = LoggerFactory.getLogger(StocksyApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(StocksyApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		LOG.info("STOCKSY -> API - INICIALIZADO COM SUCESSO!");
+	}
 }
